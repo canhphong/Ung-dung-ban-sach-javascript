978acac543d7e56b0fe8112dee2931e32e60add0
phonglecanh <phonglecanh@gmail.com>
2018-08-14 11:34:41 +0700

    edit cong nghe phonglecanh

--- Dich_vu/Xu_ly/XL_LUU_TRU.js
+++ Dich_vu/Xu_ly/XL_LUU_TRU.js
@@ -2,8 +2,7 @@ var File = require("fs")
 var Thu_muc_Du_lieu = "Du_lieu"
 var Thu_muc_Media = "Media"
 var Thu_muc_PDF = "Tap_tin"
-var Cong_nghe = "ruby"
-// var Cong_nghe = "json"
+var Cong_nghe = "phonglecanh"
 function decodeBase64Image(dataString) {
   var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
     response = {};
