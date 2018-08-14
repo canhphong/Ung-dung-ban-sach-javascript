7abfd3f3816e5691c07518909e319cf531a11ae9
canhphong <canhphong650@gmail.com>
2018-08-14 10:23:03 +0700

    sua cong nghe thanh xml by phong

--- Dich_vu/Xu_ly/XL_LUU_TRU.js
+++ Dich_vu/Xu_ly/XL_LUU_TRU.js
@@ -2,7 +2,7 @@ var File = require("fs")
 var Thu_muc_Du_lieu = "Du_lieu"
 var Thu_muc_Media = "Media"
 var Thu_muc_PDF = "Tap_tin"
-var Cong_nghe = "json"
+var Cong_nghe = "xml"
 
 function decodeBase64Image(dataString) {
   var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
